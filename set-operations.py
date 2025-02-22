6e7dd471e922a5172d63c573ae77414cfb3dc999
Bucci-beep <kibuchikahuthu@gmail.com>
2025-02-22 08:42:04 +0300

    Learning set operations in Python

---
+++ set-operations.py
@@ -0,0 +1,23 @@
+# Cast a List to a Set 
+# Cast the following list to a set:
+['A','B','C','A','B','C']
+set_1 = set(['A','B','C'])
+print(set_1)
+
+'''
+Add an Element to the Set
+Add the string 'D' to the set S.
+'''
+S = {'A','B','C'}
+S.add('D')  
+print(S)
+
+'''
+Intersection of Sets
+Find the intersection of set A and B
+'''
+A = {1,2,3,4,5}
+B = {1,3,9, 12}
+C = A & B
+print(C)
+
