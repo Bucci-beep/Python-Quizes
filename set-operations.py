c2ff3dab2a767e400dee29eb0d5a5870457cfbee
Bucci-beep <kibuchikahuthu@gmail.com>
2025-02-22 09:01:51 +0300

    Added set operations

--- set-operations.py
+++ set-operations.py
@@ -21,3 +21,26 @@ B = {1,3,9, 12}
 C = A & B
 print(C)
 
+'''
+Convert the list ['rap','house','electronic music', 'rap'] to a set:
+'''
+list_1 = ['rap','house','electronic music', 'rap']
+set_2 = set(list_1)
+print(set_2)
+
+'''
+Consider the list A = [1, 2, 2, 1] and set B = set([1, 2, 2, 1]), does sum(A) = sum(B)
+'''
+A = [1, 2, 2, 1]
+B = set([1, 2, 2, 1])
+print(sum(A))
+print(sum(B))
+
+''' 
+Create a new set album_set3 that is the union of album_set1 and album_set2:
+'''
+album_set1 = set(["Thriller", 'AC/DC', 'Back in Black'])
+album_set2 = set([ "AC/DC", "Back in Black", "The Dark Side of the Moon"])
+album_set3 = album_set1.union(album_set2)
+print(album_set3)
+
