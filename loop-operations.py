28229ff3503f1f8e1ace9658cbd983e3e061a2db
Bucci-beep <kibuchikahuthu@gmail.com>
2025-02-26 16:53:44 +0300

    Added extra real-world examples to the loop-operations.py file

--- loop-operations.py
+++ loop-operations.py
@@ -37,4 +37,31 @@ print("Multiplication Table of 7")
 for i in range(1, 11):
     print(f"7 * {i} = {7*i}")
 
-    
+'''Use a for loop to create a list of natural numbers up to 10 and print the list'''
+print("List of Natural Numbers up to 10")
+natural_numbers = []
+for i in range(1, 11):
+    natural_numbers.append(i)
+print(natural_numbers)
+
+'''Use a for loop to create a list of even numbers up to 10 and print the list'''
+print("List of Even Numbers up to 10")
+even_numbers = []
+for i in range(2, 11, 2):
+    even_numbers.append(i)
+print(even_numbers)
+
+'''Use a for loop to create a list of numbers up to 10 that are divisible by 3 and print the list'''
+print("List of Numbers up to 10 that are divisible by 3")
+divisible_by_3 = []
+for i in range(3, 11, 3):
+    divisible_by_3.append(i)
+print(divisible_by_3)
+
+'''Use a for loop to create a list of numbers up to 10 that are not divisible by 3 and print the list'''
+print("List of Numbers up to 10 that are not divisible by 3")
+not_divisible_by_3 = []
+for i in range(1, 11):
+    if i % 3 != 0:
+        not_divisible_by_3.append(i)
+print(not_divisible_by_3)
